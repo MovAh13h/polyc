b4f4ace6afe0a538e1176b4b544900a309acc140
Tanishq Jain <tanishqjain1002@gmail.com>
2026-07-11 00:02:03 +0530

    add dialect

---
+++ include/polyc/Dialect/Poly/PolyDialect.h
@@ -0,0 +1,8 @@
+#ifndef POLYC_DIALECT_POLY_POLYDIALECT_H
+#define POLYC_DIALECT_POLY_POLYDIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "polyc/Dialect/Poly/PolyDialect.h.inc"
+
+#endif  // POLYC_DIALECT_POLY_POLYDIALECT_H
---
+++ lib/Dialect/Poly/PolyDialect.cc
@@ -0,0 +1,7 @@
+#include "polyc/Dialect/Poly/PolyDialect.h"
+#include "polyc/Dialect/Poly/PolyDialect.cc.inc"
+
+using namespace mlir;
+using namespace mlir::poly;
+
+void PolyDialect::initialize() {}
